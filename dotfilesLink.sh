c63818fa1201419b6e3d8318edc12dccf9da959a
haru <haru@inl.ics.keio.ac.jp>
2017-09-25 18:24:02 +0900

    Initialize repository

---
+++ dotfilesLink.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
+# ln -sf ~/dotfiles/.bashrc ~/.bashrc
+# ln -sf ~/dotfiles/.zprofile ~/.zprofile
+# ln -sf ~/dotfiles/.zshrc ~/.zshrc
+ln -sf ~/dotfiles/.vimrc ~/.vimrc
